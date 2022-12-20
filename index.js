d5b63b2d803dcab20a307a820c1dfba1b6029e7a
Xenovka <wendyanto18@gmail.com>
2022-12-20 21:24:46 +0700

    new: enemy, collision, explosion effect, etc

--- index.js
+++ index.js
@@ -81,9 +81,6 @@ class Enemy {
     this.velocity = velocity;
   }
 
-  /**
-   * * function for drawing/displaying the Projectile instance.
-   */
   draw() {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
@@ -91,13 +88,41 @@ class Enemy {
     c.fill();
   }
 
-  /**
-   * * used for updating the x & y position of the Projectile on every frame.
-   */
   update() {
     this.draw();
     this.x = this.x + this.velocity.x;
     this.y = this.y + this.velocity.y;
+  }
+}
+
+const friction = 0.99;
+class Particles {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+    this.alpha = 1;
+  }
+
+  draw() {
+    c.save();
+    c.globalAlpha = this.alpha;
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.restore();
+  }
+
+  update() {
+    this.draw();
+    this.velocity.x *= friction;
+    this.velocity.y *= friction;
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+    this.alpha -= 0.01;
   }
 }
 
@@ -108,13 +133,14 @@ class Enemy {
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(x, y, 30, "blue");
+const player = new Player(x, y, 15, "white");
 const projectiles = [];
 const enemies = [];
+const particles = [];
 
 function spawnEnemies() {
   setInterval(() => {
-    const radius = Math.random() * (30 - 4) + 4;
+    const radius = Math.random() * (30 - 8) + 8;
 
     let x;
     let y;
@@ -126,7 +152,7 @@ function spawnEnemies() {
       x = Math.random() * canvas.width;
       y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
     }
-    const color = "green";
+    const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
 
     const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
 
@@ -139,16 +165,82 @@ function spawnEnemies() {
   }, 1000);
 }
 
+let animationId;
+
 function animate() {
-  requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
-  projectiles.forEach((projectile) => {
+  animationId = requestAnimationFrame(animate);
+  c.fillStyle = "rgba(0,0,0, .1)";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  particles.forEach((particle, index) => {
+    if (particle.alpha <= 0) {
+      particles.splice(index, 1);
+    } else {
+      particle.update();
+    }
+  });
+  projectiles.forEach((projectile, index) => {
     projectile.update();
+
+    /**
+     * * Removing projectile that already off from the screen
+     */
+    if (
+      projectile.x + projectile.radius < 0 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+    ) {
+      setTimeout(() => {
+        projectiles.splice(index, 1);
+      }, 0);
+    }
   });
   player.draw();
 
-  enemies.forEach((enemy) => {
+  enemies.forEach((enemy, index) => {
     enemy.update();
+
+    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+
+    if (dist - enemy.radius - player.radius < 1) {
+      cancelAnimationFrame(animationId);
+    }
+
+    projectiles.forEach((projectile, projectileIndex) => {
+      const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+
+      /**
+       * * Remove/ destory enemy when the projectile hit
+       */
+      if (dist - enemy.radius - projectile.radius < 1) {
+        // Create explosion effect
+        for (let i = 0; i < enemy.radius * 2; i++) {
+          particles.push(
+            new Particles(projectile.x, projectile.y, Math.random() * 2, enemy.color, {
+              x: (Math.random() - 0.5) * (Math.random() * 4),
+              y: (Math.random() - 0.5) * (Math.random() * 4)
+            })
+          );
+        }
+
+        /**
+         * * Shrinking enemies that have radius more than 5
+         */
+        if (enemy.radius - 10 > 5) {
+          gsap.to(enemy, {
+            radius: enemy.radius - 10
+          });
+          setTimeout(() => {
+            projectiles.splice(projectileIndex, 1);
+          }, 0);
+        } else {
+          setTimeout(() => {
+            enemies.splice(index, 1);
+            projectiles.splice(projectileIndex, 1);
+          }, 0);
+        }
+      }
+    });
   });
 }
 
@@ -163,11 +255,11 @@ window.addEventListener("click", (e) => {
    */
   const angle = Math.atan2(e.clientY - canvas.height / 2, e.clientX - canvas.width / 2);
   const velocity = {
-    x: Math.cos(angle),
-    y: Math.sin(angle)
+    x: Math.cos(angle) * 4,
+    y: Math.sin(angle) * 4
   };
 
-  projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity));
+  projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, "white", velocity));
 });
 
 animate();
